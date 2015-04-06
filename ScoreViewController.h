d2dcf2947b9fa9072a327b7a07fd4b62d38da20a
rosewebdev <jason@rosewebdev.com>
2015-04-05 23:39:40 -0600

    Step 1.2 added class called ScoreViewController

---
+++ ScoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScoreViewController.h
+//  Score Keeper
+//
+//  Created by Jason Kupiec on 4/5/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoreViewController : UIViewController
+
+@end
---
+++ ScoreViewController.m
@@ -0,0 +1,37 @@
+//
+//  ScoreViewController.m
+//  Score Keeper
+//
+//  Created by Jason Kupiec on 4/5/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ScoreViewController.h"
+
+@interface ScoreViewController ()
+
+@end
+
+@implementation ScoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
