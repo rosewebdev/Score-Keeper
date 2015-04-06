fe443dfea038649339b60c64903b78637914e1bc
rosewebdev <jason@rosewebdev.com>
2015-04-05 23:47:58 -0600

    Step 2.2 added ScrollView and initialized

--- ScoreViewController.m
+++ ScoreViewController.m
@@ -20,6 +20,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    UIScrollView *scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 64)];
+    [self.view addSubview:scrollView];
 }
 
 - (void)didReceiveMemoryWarning {
